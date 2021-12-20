f6158e4ba4a4b622c6c0d8bb501515f93472e9ef
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-12-20 13:26:03 +0300

    MarketContext

--- README.md
+++ README.md
@@ -13,8 +13,13 @@
 ```
 /ApplicationCore
 Install-Package Ardalis.Specification
+
+/Infrastructure
+Install-Package Microsoft.EntityFrameworkCore -v 5.0.13
 ```
 
 ### Resources
 * https://github.com/dotnet-architecture/eShopOnWeb
 * https://github.com/yigith/WatchShop
+* https://www.postgresql.org
+* https://www.npgsql.org/doc/types/basic.html
---
+++ src/Infrastructure/Data/MarketContext.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Infrastructure.Data
+{
+    public class MarketContext : DbContext
+    {
+        public MarketContext(DbContextOptions<MarketContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
