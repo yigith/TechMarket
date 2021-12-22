2ceb3b9e4d63df8f6d4d3b7f8b95d7aa07396e59
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-12-22 12:53:34 +0300

    home view model

---
+++ src/Web/Services/HomeViewModelService.cs
@@ -0,0 +1,19 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Services
+{
+    public class HomeViewModelService
+    {
+        private readonly IRepository<Product> _productRepository;
+
+        public HomeViewModelService(IRepository<Product> productRepository)
+        {
+            _productRepository = productRepository;
+        }
+    }
+}
