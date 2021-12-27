23a3af2ecd08ce4898c1f953fcb178af5a46fccb
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-12-27 13:34:23 +0300

    basket & basket item

---
+++ src/Web/Controllers/BasketController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+    public class BasketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public static int adet = 0;
+        [HttpPost]
+        public IActionResult AddItem(int productId)
+        {
+            return Json(++adet);
+        }
+    }
+}
