b4007994528ea43515b1894dfbec61214226cdfc
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-12-27 11:14:57 +0300

    pagination

--- README.md
+++ README.md
@@ -43,3 +43,5 @@ Update-Database -Context AppIdentityDbContext -s Web
 * https://www.npgsql.org/efcore/
 * https://getbootstrap.com/docs/4.6/components/card/#example
 * https://stackoverflow.com/questions/5269713/css-ellipsis-on-second-line
+* https://getbootstrap.com/docs/4.6/components/pagination/#alignment
+* https://gist.github.com/yigith/c6f999788b833dc3d22ac6332a053dd1
--- src/Web/Models/PaginationInfoViewModel.cs
+++ src/Web/Models/PaginationInfoViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Web.Models
 {
@@ -29,5 +30,44 @@ namespace Web.Models
         public bool HasPrevious => CurrentPage > 1;
 
         public bool HasNext => CurrentPage < TotalPages;
+
+        public int[] PageNumbers => Pagination(CurrentPage, TotalPages);
+
+        private static int[] Pagination(int current, int last)
+        {
+            int delta = 1;
+            int left = current - delta;
+            int right = current + delta + 1;
+            var range = new List<int>();
+            var rangeWithDots = new List<int>();
+            int? l = null;
+
+            for (var i = 1; i <= last; i++)
+            {
+                if (i == 1 || i == last || i >= left && i < right)
+                {
+                    range.Add(i);
+                }
+            }
+
+            foreach (var i in range)
+            {
+                if (l != null)
+                {
+                    if (i - l == 2)
+                    {
+                        rangeWithDots.Add(l.Value + 1);
+                    }
+                    else if (i - l != 1)
+                    {
+                        rangeWithDots.Add(-1);
+                    }
+                }
+                rangeWithDots.Add(i);
+                l = i;
+            }
+
+            return rangeWithDots.ToArray();
+        }
     }
 }
